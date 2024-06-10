e55df077009bfabbb9e15b2c6b75cb98c8b6e3de
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-06-10 11:06:51 +0300

    build: added output and entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
-module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          // транспиляция js
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  /**
-   * Теперь при просмотре в логах кода можно посмотреть файл в обычном виде, а не в транспилированном.
-   * Это происходит  благодаря файлу main.js.map
-   */
-  devtool: "source-map",
+const path = require("path");
 
-  devServer: {
-    static: {
-      directory: "./dist",
+const mode = process.env.NODE_ENV;
+
+module.exports = (env) => {
+  console.log(env);
+  return {
+    mode: env.NODE_ENV === "production" ? "production" : "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "public"),
     },
-  },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            // транспиляция js
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    /**
+     * Теперь при просмотре в логах кода можно посмотреть файл в обычном виде, а не в транспилированном.
+     * Это происходит  благодаря файлу main.js.map
+     */
+    devtool: "source-map",
+
+    devServer: {
+      static: {
+        directory: "./public",
+      },
+    },
+  };
 };
