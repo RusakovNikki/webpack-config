e5a0ba04288cabc040a8e5d67dc4c6eb4766052d
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-06-10 09:57:09 +0300

    build: добавление source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 module.exports = {
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
+          // транспиляция js
           loader: "babel-loader",
         },
       },
     ],
   },
+  /**
+   * Теперь при просмотре в логах кода можно посмотреть файл в обычном виде, а не в транспилированном.
+   * Это происходит  благодаря файлу main.js.map
+   */
+  devtool: "source-map",
 };
