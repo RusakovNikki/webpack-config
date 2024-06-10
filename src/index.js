fb7e57d396be8907eaff37ed3f9730b42aebba1a
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-06-10 09:49:46 +0300

    build: транспиляция через babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import { getClasses } from "./getClasses";
 
 getClasses();
+
+const obj = {
+  a: "Alpha",
+  b: "Beta",
+};
+
+const newObg = { ...obj, c: "Charlie" };
+/**
+ * Происходит транспиляция. Разница между транспиляцией и компиляцией в том,
+ * что компиляция - перевод на другой язык. А транспиляция - перевод на тот же язык, но более старую версию
+ */
+console.log(newObg);
