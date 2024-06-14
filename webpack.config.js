be70ae9029507b14721598237e8bbdf8c3464540
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-06-14 17:34:27 +0300

    removed public folder

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
     entry: "./src/index.js",
     output: {
       filename: "bundle.js",
-      path: path.resolve(__dirname, "public"),
+      path: path.resolve(__dirname, "dist"),
     },
 
     module: {
@@ -54,7 +54,7 @@ module.exports = (env) => {
 
     devServer: {
       static: {
-        directory: "./public",
+        directory: "./dist",
       },
     },
   };
