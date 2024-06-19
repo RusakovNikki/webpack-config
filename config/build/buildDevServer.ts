970b77ec4e90b5382b88d2cf510e304fb85285de
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-06-19 18:31:05 +0300

    build: working with React Router Dom

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -9,4 +9,10 @@ export const buildDevServer = (
   //   },
   port: options.port ?? 3000,
   open: true,
+  /**
+   * Чтобы поддерживался роутинг в React Router Dom.
+   * Работает только для devServer.
+   * Если раздавать статику через nginx то нужно делать проксирование на index.html
+   */
+  historyApiFallback: true,
 });
